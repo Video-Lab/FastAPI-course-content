a0034b9640a24e147e8fcd38a4df774be76629ca
Jad Khalili <jad@videolab.ae>
2021-10-31 13:39:56 -0500

    Design a social media post model w/ Pydantic

---
+++ 2_advanced_python_and_concepts/coding_ex_1.py
@@ -0,0 +1,15 @@
+"""
+Your goal is to create a social media post model using pydantic. The model should have:
+An author, which is a string
+An optional co-author, which is a string
+A date, which is a string
+A title, which is a string
+The content, which is a string
+An ID, which is an integer
+Likes, which is a list of strings
+
+The post should also have a field for comments, which is a list of comment models. The model should have:
+An author, which is a string
+The comment, which is a string
+Likes, which is an integer
+"""
