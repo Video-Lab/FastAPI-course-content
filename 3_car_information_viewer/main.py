371df20fab4357da659f3c961182392d2c46c399
Jad Khalili <jad@videolab.ae>
2021-10-31 16:36:24 -0500

    Car Information Viewer: Updating cars

--- 3_car_information_viewer/main.py
+++ 3_car_information_viewer/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI, Query, Path, HTTPException, status, Body
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field
 from typing import Optional, List, Dict
 
 from starlette.status import HTTP_400_BAD_REQUEST
 from database import cars
 class Car(BaseModel):
-    make: str
-    model: str
-    year: int = Field(...,ge=1970,lt=2022)
-    price: float
+    make: Optional[str]
+    model: Optional[str]
+    year: Optional[int] = Field(...,ge=1970,lt=2022)
+    price: Optional[float]
     engine: Optional[str] = "V4"
-    autonomous: bool
-    sold: List[str]
+    autonomous: Optional[bool]
+    sold: Optional[List[str]]
 
 app = FastAPI()
 
@@ -45,3 +46,16 @@ def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
             min_id += 1
         cars[min_id] = car
         min_id += 1
+
+@app.put("/cars/{id}",response_model=Dict[str,Car])
+def update_car(id: int, car: Car = Body(...)):
+    stored = cars.get(id)
+    if not stored:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could not find car with given ID.")
+    stored = Car(**stored)
+    new = car.dict(exclude_unset=True)
+    new = stored.copy(update=new)
+    cars[id] = jsonable_encoder(new)
+    response = {}
+    response[id] = cars[id]
+    return response
