99e49b612a892094871d21de5e1afe9786a02e11
Jad Khalili <jad@videolab.ae>
2021-10-31 16:04:14 -0500

    Car Information Viewer: Get car by ID

--- 3_car_information_viewer/main.py
+++ 3_car_information_viewer/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path, HTTPException, status
 from pydantic import BaseModel, Field
 from typing import Optional, List, Dict
 from database import cars
@@ -25,3 +25,10 @@ def get_cars(number: Optional[str] = Query("10",max_length=3)):
         to_add[id] = car
         response.append(to_add)
     return response
+
+@app.get("/cars/{id}", response_model=Car)
+def get_car_by_id(id: int = Path(...,ge=0,lt=1000)):
+    car = cars.get(id)
+    if not car:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could not find car by ID.")
+    return car
