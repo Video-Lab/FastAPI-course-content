e59920fa69494589f6e4fdbe8cc2b3f399e3c889
Jad Khalili <jad@videolab.ae>
2021-10-31 14:25:57 -0500

    Type hints lecture

---
+++ 2_advanced_python_and_concepts/type_hints.py
@@ -0,0 +1,4 @@
+def add_integers(i: int, j: int): # Format | parameter: type (type can be int, str, bool, ...)
+    return i + j # Editor support added
+
+print(add_integers('ab','cd')) # Doesn't actually restrict anything
