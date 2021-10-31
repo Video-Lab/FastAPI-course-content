8204ce9a746fff8a4217cc19ed058e34f072b2d6
Jad Khalili <jad@videolab.ae>
2021-10-31 15:38:29 -0500

    Car Information Viewer: Creating a car model

--- 3_car_information_viewer/main.py
+++ 3_car_information_viewer/main.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional, List
+
+class Car(BaseModel):
+    make: str
+    model: str
+    year: int = Field(...,ge=1970,lt=2022)
+    price: float
+    engine: Optional[str] = "V4"
+    autonomous: bool
+    sold: List[str]
 
 app = FastAPI()
 
