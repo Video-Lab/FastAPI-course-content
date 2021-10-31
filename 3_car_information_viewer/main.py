40dc3fb49792cd984b4ac1dd7b488a2e2bafd7e8
Jad Khalili <jad@videolab.ae>
2021-10-31 16:14:31 -0500

    Car Information Viewer: Adding cars

--- 3_car_information_viewer/main.py
+++ 3_car_information_viewer/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Query, Path, HTTPException, status
+from fastapi import FastAPI, Query, Path, HTTPException, status, Body
 from pydantic import BaseModel, Field
 from typing import Optional, List, Dict
+
+from starlette.status import HTTP_400_BAD_REQUEST
 from database import cars
 class Car(BaseModel):
     make: str
@@ -32,3 +34,14 @@ def get_car_by_id(id: int = Path(...,ge=0,lt=1000)):
     if not car:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could not find car by ID.")
     return car
+
+@app.post("/cars", status_code=status.HTTP_201_CREATED)
+def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
+    if len(body_cars) < 1:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST,detail="No cars to add.")
+    min_id = len(cars.values()) + min_id
+    for car in body_cars:
+        while cars.get(min_id):
+            min_id += 1
+        cars[min_id] = car
+        min_id += 1
