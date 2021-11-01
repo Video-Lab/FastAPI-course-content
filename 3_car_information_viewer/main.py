a02150a1f2c3c28c4be2ada0c1563b5b56b1cbe1
Jad Khalili <jad@videolab.ae>
2021-11-01 13:28:40 -0500

    Car Information Viewer: Creating the main page

--- 3_car_information_viewer/main.py
+++ 3_car_information_viewer/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Query, Path, HTTPException, status, Body, Request
 from fastapi.encoders import jsonable_encoder
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel, Field
@@ -23,18 +24,18 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=RedirectResponse)
 def root(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request, "title": "FastAPI - Home"})
+    return RedirectResponse(url="/cars")
 
-@app.get("/cars", response_model=List[Dict[str, Car]])
-def get_cars(number: Optional[str] = Query("10",max_length=3)):
+@app.get("/cars", response_class=HTMLResponse)
+def get_cars(request: Request, number: Optional[str] = Query("10",max_length=3)):
     response = []
     for id, car in list(cars.items())[:int(number)]:
         to_add = {}
         to_add[id] = car
         response.append(to_add)
-    return response
+    return templates.TemplateResponse("index.html", {"request": request, "cars": response, "title": "Home"})
 
 @app.get("/cars/{id}", response_model=Car)
 def get_car_by_id(id: int = Path(...,ge=0,lt=1000)):
