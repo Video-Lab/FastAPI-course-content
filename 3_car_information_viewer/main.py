a3a246e92d230de02e107ccb56de1166b16f3904
Jad Khalili <jad@videolab.ae>
2021-11-01 15:36:05 -0500

    Car Information Viewer: Delete car

--- 3_car_information_viewer/main.py
+++ 3_car_information_viewer/main.py
@@ -100,8 +100,9 @@ def update_car(request: Request, id: int,
     response[id] = cars[id]
     return RedirectResponse(url="/cars", status_code=302)
 
-@app.delete("/cars/{id}")
-def delete_car(id: int):
+@app.get("/delete/{id}", response_class=RedirectResponse)
+def delete_car(request: Request, id: int = Path(...)):
     if not cars.get(id):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could not find car with given ID.")
+        return templates.TemplateResponse("search.html", {"request": request, "id": id, "title": "Edit Car"}, status_code=status.HTTP_404_NOT_FOUND)
     del cars[id]
+    return RedirectResponse(url="/cars")
