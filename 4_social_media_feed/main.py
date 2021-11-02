24c732819da0e1a58eef7c3c9bead1743d7c1067
Jad Khalili <jad@videolab.ae>
2021-11-02 18:26:54 -0500

    Social Media Feed: Setting up app

---
+++ 4_social_media_feed/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def root():
+    return {"Set": "up!"}
