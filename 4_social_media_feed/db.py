1a7de21d5c025478f363964756edc4e62c6b8190
Jad Khalili <jad@videolab.ae>
2021-11-02 18:16:53 -0500

    Social Media Feed: Initial

---
+++ 4_social_media_feed/db.py
@@ -0,0 +1,46 @@
+users = {
+    "jadkhalili": {
+        "name": "Jad Khalili",
+        "username": "jadkhalili",
+        "email": "jad@email.com",
+        "birthday": "1st January 1970",
+        "friends": ["johndoe", "janedoe1", "doe.jim95"],
+        "notifications": [
+            {
+                "author": "janedoe1",
+                "description": "liked a post you made."
+            },
+            {
+                "author": "doe.jim95",
+                "description": "Messaged you: \"Wanna meet up this weekend?\""
+            },
+            {
+                "author": "johndoe",
+                "description": "turns a year older today. Wish them a happy birthday!"
+            },
+        ],
+        "hashed_password": "$2b$12$qklRsJyj.PAVzezU7vfj5uBjaelr7lSIFqrNT.7CnJEPkRi7nstxO"
+    },
+    "johndoe": {
+        "name": "John Doe",
+        "username": "johndoe",
+        "email": "johndoe@email.com",
+        "birthday": "31st December 1999",
+        "friends": ["jadkhalili", "lip.sum"],
+        "notifications": [
+            {
+                "author": "jadkhalili",
+                "description": "Messaged you: \"k\""
+            },
+            {
+                "author": "lip.sum",
+                "description": "tagged you in a post."
+            },
+            {
+                "author": "kwurtea",
+                "description": "Messaged you: \"you there???\""
+            },
+        ],
+        "hashed_password": "$2b$12$4SqrDVzv6w2wRAbcdVxCdu.zrDJjk/TVWYeStP2V8odpKNDtHqgA."
+    }
+}
