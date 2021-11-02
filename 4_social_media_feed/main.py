ae4f3a8d1c7feea26e7e83a7856a711b78deea42
Jad Khalili <jad@videolab.ae>
2021-11-02 18:33:18 -0500

    Social Media Feed: Creating models

--- 4_social_media_feed/main.py
+++ 4_social_media_feed/main.py
@@ -2,6 +2,24 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from typing import List, Optional
+from db import users
+
+class Notification(BaseModel):
+    author: str
+    description: str
+
+class User(BaseModel):
+    name: str
+    username: str
+    email: str
+    birthday: str
+    friends: List[str]
+    notifications: List[Notification]
+
+class UserDB(User):
+    hashed_password: str
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
