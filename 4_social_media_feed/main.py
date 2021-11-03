cb9e50449e09e169df25ed4ae7b2c81d0be4640a
Jad Khalili <jad@videolab.ae>
2021-11-03 16:28:55 -0500

    Social Media Feed: Login data rout

--- 4_social_media_feed/main.py
+++ 4_social_media_feed/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Response, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
@@ -9,6 +10,7 @@ from passlib.context import CryptContext
 from fastapi_login import LoginManager
 import os
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
@@ -35,8 +37,8 @@ pwd_ctx = CryptContext(schemes=["bcrypt"],deprecated="auto")
 def get_hashed_password(plain_password):
     return pwd_ctx.hash(plain_password)
 
-def verify_password(plain_plassword, hashed_password):
-    return pwd_ctx.verify(plain_plassword,hashed_password)
+def verify_password(plain_password, hashed_password):
+    return pwd_ctx.verify(plain_password,hashed_password)
 
 class Notification(BaseModel):
     author: str
@@ -63,4 +65,31 @@ def root(request: Request):
 
 @app.get("/login", response_class=HTMLResponse)
 def get_login(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request, "title": "FriendConnect - Login", "invalid": True})
+    return templates.TemplateResponse("login.html", {"request": request, "title": "FriendConnect - Login"})
+
+@app.post("/login")
+def login(request: Request, response: Response, form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(username=form_data.username, password=form_data.password)
+    if not user:
+        return templates.TemplateResponse("login.html", {"request": request, "title": "FriendConnect - Login", "invalid": True}, status_code=status.HTTP_401_UNAUTHORIZED)
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
+    access_token = manager.create_access_token(
+        data={"sub": user.username},
+        expires=access_token_expires
+    )
+    resp = RedirectResponse("/home", status_code=status.HTTP_302_FOUND)
+    manager.set_cookie(resp,access_token)
+    return resp
+
+class NotAuthenticatedException(Exception):
+    pass
+
+def not_authenticated_exception_handler(request, exception):
+    return RedirectResponse("/login")
+
+manager.not_authenticated_exception = NotAuthenticatedException
+app.add_exception_handler(NotAuthenticatedException, not_authenticated_exception_handler)
+
+@app.get("/home")
+def home(user: User = Depends(manager)):
+    return user
