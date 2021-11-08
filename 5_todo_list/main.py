0f4e4907c41edfb69e2fbe36962b88f77675907c
Jad Khalili <jad@videolab.ae>
2021-11-08 12:16:38 -0600

    Todo List: Setting up app

---
+++ 5_todo_list/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def root():
+    return {"Set": "up!"}
