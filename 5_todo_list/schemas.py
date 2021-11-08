078b5cbc1d36bc8cb3301fd783b4bfd5700b4833
Jad Khalili <jad@videolab.ae>
2021-11-08 13:08:06 -0600

    Creating Pydantic models (schemas)

---
+++ 5_todo_list/schemas.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class TaskBase(BaseModel):
+    text: str
+
+class Task(TaskBase):
+    id: str
+    user_id: str
+
+    class Config:
+        orm_mode = True
+
+class TaskCreate(TaskBase):
+    pass
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+    hashed_password: str
+
+class User(UserBase):
+    id: str
+    tasks: List[Task] = []
+
+    class Config:
+        orm_mode = True
+
+class UserCreate(UserBase):
+    pass
