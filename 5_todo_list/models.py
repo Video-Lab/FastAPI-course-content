0056407e636e201e31ec32c91ef19d390e610b6f
Jad Khalili <jad@videolab.ae>
2021-11-08 13:24:05 -0600

    Todo List: Create database models

---
+++ 5_todo_list/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
+from db import Base
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(String, unique=True, primary_key=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String,nullable=False)
+
+    items = relationship("Task", back_populates="user")
+
+class Task(Base):
+    __tablename__ = "task"
+
+    id = Column(String, primary_key=True, index=True, nullable=False)
+    text = Column(String, index=True, nullable=False)
+    user_id = Column(String, ForeignKey("user.id"), nullable=False)
+
+    user = relationship("User", back_populates="task")
