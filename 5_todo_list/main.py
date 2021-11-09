45eed749478babb1c3d78e403824351c8d40ace4
Jad Khalili <jad@videolab.ae>
2021-11-09 17:26:23 -0600

    Todo List: App setup with DB

--- 5_todo_list/main.py
+++ 5_todo_list/main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.encoders import jsonable_encoder
+from db import SessionLocal, engine, DBContext
+import models
+from sqlalchemy.orm import Session
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+def get_db():
+    with DBContext() as db:
+        yield db
+
 @app.get("/")
 def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "title": "Home"})
+
+@app.get("/tasks")
+def get_tasks(db: Session = Depends(get_db)):
+    return jsonable_encoder(db.query(models.Task).first())
---
+++ 5_todo_list/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from db import Base
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(String, primary_key=True, index=True,nullable=False)
+    email = Column(String, unique=True, index=True,nullable=False)
+    name = Column(String, index=True)
+    username = Column(String, unique=True, index=True,nullable=False)
+    hashed_password = Column(String,nullable=False)
+
+    items = relationship("Task", back_populates="user")
+
+
+class Task(Base):
+    __tablename__ = "item"
+
+    id = Column(String, primary_key=True, index=True,nullable=False)
+    text = Column(String, index=True,nullable=False)
+    user_id = Column(String, ForeignKey("user.id"),nullable=False)
+
+    user = relationship("User", back_populates="items")
